5acc30b09582058157f8a36d3c81a221f3a5f4e5
DrSemFrankenstein <DrSemFrankenstein>
2024-10-17 01:36:32 +0300

    vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  base: "/blood-pressure/",
+  base: "/blood-pressure/", 
   plugins: [
     react(),
     VitePWA({
@@ -15,15 +15,15 @@ export default defineConfig({
         theme_color: "#ffffff",
         background_color: "#ffffff",
         display: "standalone",
-        start_url: "/blood-pressure/",
+        start_url: "/blood-pressure/", 
         icons: [
           {
-            src: "/android-chrome-192x192.png",
+            src: "/blood-pressure/android-chrome-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/android-chrome-512x512.png",
+            src: "/blood-pressure/android-chrome-512x512.png", 
             sizes: "512x512",
             type: "image/png",
           },
